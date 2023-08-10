b2558fb1601277111c3007658908f6a66b1612fb
Viper292 <crazymelon@hotmail.es>
2023-08-10 19:12:43 -0300

    Adds basic homepage
    The first thing the user sees, will be modified to be more intuitive down the line

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-
+import Home from "./components/Home";
 
 export default function App() {
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello world!
-    </h1>
+    <div className="main">
+      <img className="w-full h-screen object-cover" draggable="false" src="/images/background.png" />
+      <div className="w-full h-screen absolute top-0 left-0 flex items-center justify-center">
+        <Home />
+      </div>
+    </div>
   )
 }
