df3246cf50eb28a08faae118c9ad00ad5a324b20
Viper292 <crazymelon@hotmail.es>
2023-09-04 10:18:15 -0300

    Adds species portraits

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
-
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-full">
-      <p>Welcome to the stellaris app!</p>
+    <div className="flex flex-col justify-center items-center h-full">
+      <p>Welcome to the Stellaris Empire Creator!</p>
+      <p>This app was made by <b><a href="https://github.com/Nullim">Nullim</a></b></p>
     </div>
   )
 }
